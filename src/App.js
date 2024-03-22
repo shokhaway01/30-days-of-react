cb8de3302c29bedba42159353cf2137aab47696a
shokhaway01 <shoxauey@gmail.com>
2024-03-23 00:02:51 +0500

    new files were added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Shokhaway XD
+          Learn Shokhaway
         </a>
       </header>
     </div>
