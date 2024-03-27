18036ebfa4598dd44d6e9c6a03184e9529e00b1e
shokhaway01 <shoxauey@gmail.com>
2024-03-27 22:04:48 +0500

    new commits

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-
-import Header from './components/header';
-import Navbar from './components/navbar';
-import Main from './components/main';
 
 function App() {
   return (
-    <div className='all'>
-      <Header />
-      <div className='body'>
-        <Navbar />
-        <Main />  
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/components/navbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../Assets/styles/Navbar.css'
-const Navbar = () => {
-  return (
-    <div className='nav'>navbar</div>
-  )
-}
-
-export default Navbar
