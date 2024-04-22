249dc53a686be1e44a88c30fa8478000a9e2aada
shokhaway01 <shoxauey@gmail.com>
2024-04-22 11:58:19 +0500

    hello world

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
-let novey = Array(10).fill('Sardor');
-
-console.log(novey);
-
-
 let newLine = 'Hello my name is kitty!'
 let newLineArr = newLine.split(' ');
 console.log(newLineArr);
+// Тут вы можете заметить как работает метод сплит/split
+
+let asli = Array(5).fill('xxx')
+// console.log(asli);
+
+// А тут вы можете наблюдать как наш пустой массив заполняется статичными данными
+
+
+// Чтобы вы могли прибавлять или объединить два или более массивов то вы можете использовать метод concat
+
+let first = Array(1).fill(1);
+let second = Array(1).fill(2);
+let third = Array(1).fill(3);
+
+let both = first.concat(second.concat(third));
+
+// console.log(both);
+
+
+let berries = ['Strawberries', 'Bluebery','cherry'];
+let exotics = ['banana','apple', 'orange','pineapple']
+
+let fruits = berries.concat(exotics);
+
+let index = fruits.indexOf('Bluebery');
+console.log(index);
+
+index != -1 
+    ? console.log('This fruit does exist in the array')
+    : console.log('Does not exist!!!');
