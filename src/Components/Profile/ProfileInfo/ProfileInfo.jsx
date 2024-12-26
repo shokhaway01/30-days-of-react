728595d23e907da14fb2b3e06c950b0189dc46b2
shokhaway01 <shoxauey@gmail.com>
2024-12-26 18:03:47 +0500

    commit

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -7,7 +7,7 @@ const ProfileInfo = () => {
   return (
     <div>
         <div>
-            <img className={s.content_image} src="https://avatars.mds.yandex.net/i?id=62c716cb93778ff6753e0b32ed02f54c43dc4b28-5513384-images-thumbs&n=13" alt=""/>
+            <img className={s.content_image} src="https://cdn1.ozone.ru/s3/multimedia-5/6265353017.jpg" alt=""/>
         </div>
 
         <div>
